a169fbf54767e993567958a7df84f5809be25a97
Raph <93734816+RxphGui@users.noreply.github.com>
2022-03-23 12:24:06 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,9 @@
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+
+client.once('ready', () => {
+    console.log(`le bot est on`);
+});
+
+client.on('error', console.error);
+client.login('');
